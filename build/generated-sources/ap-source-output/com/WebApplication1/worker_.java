defeefd246b235100c405497c920e169b4c11b6f
MarwanAliAlbahar <MD@marwans-MacBook-Air.local>
2017-06-27 21:57:31 +0300

    Webapp
    webApp

---
+++ build/generated-sources/ap-source-output/com/WebApplication1/worker_.java
@@ -0,0 +1,16 @@
+package com.WebApplication1;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-27T11:07:28")
+@StaticMetamodel(worker.class)
+public class worker_ { 
+
+    public static volatile SingularAttribute<worker, Integer> PoitionColumn;
+    public static volatile SingularAttribute<worker, String> PositionRow;
+    public static volatile SingularAttribute<worker, String> PlateID;
+    public static volatile SingularAttribute<worker, Long> id;
+
+}
